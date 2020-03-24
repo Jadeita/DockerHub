f9329e4a872fb68f9569ba1c9b953b663225c144
Jade Bañuelos <joibg.ver@gmail.com>
2020-03-24 15:02:36 -0600

    cambio de diseño

--- docker/miweb/index.php
+++ docker/miweb/index.php
@@ -1,5 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Titulares</title>
+	<meta charset="utf-8">
+</head>
+<body>
+<h1>Titular de tamaño h1 </h1>
+<h2>Titular de tamaño h2</h2>
+<h3>Titular de tamaño h3</h3>
+<h4>Titular de tamaño h4</h4>
+<h5>Titular de tamaño h5</h5>
+<h6>Titular de tamaño h6</h6>
+</body>
 
-        echo "Hola heyner :) ";
-
-?>
+</html> 

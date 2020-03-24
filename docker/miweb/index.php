4d4a14b6ee20274b703b62a997ff291edd059ff3
Jade Bañuelos <joibg.ver@gmail.com>
2020-03-24 16:27:28 -0600

    jj

--- docker/miweb/index.php
+++ docker/miweb/index.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 </head>
 
-<body background="imagenes/imagen1.png">
+<body background="https://lh4.googleusercontent.com/fss7r2H9QJsR9yT6J8PxhIQDPg3if5p_mRgbK0U0Ep5Q1rsi17vobpHLzbXp1oPFrIwevGt4kKU9TcX82hfM2Xe6EiqkOnU8bAkBifsxhTM5nSRaMks=w1280">
 
 </body>
 

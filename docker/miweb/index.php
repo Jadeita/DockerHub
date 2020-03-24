7cfbda890d7fab304d65f6b37731d245991a3f19
Jade Bañuelos <joibg.ver@gmail.com>
2020-03-24 15:36:11 -0600

    diseño

--- docker/miweb/index.php
+++ docker/miweb/index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> Titulares</title>
+	<title> WebServices </title>
 	<meta charset="utf-8">
 </head>
-<body>
+<body background="<?php echo base_url(); ?>imagenes/imagen1.png">
+  <font color="#FEAEE2" size="5" >
+  <center>
 <h1>Titular de tamaño h1 </h1>
+  </center>
+
 <h2>Titular de tamaño h2</h2>
 <h3>Titular de tamaño h3</h3>
 <h4>Titular de tamaño h4</h4>
@@ -13,4 +17,4 @@
 <h6>Titular de tamaño h6</h6>
 </body>
 
-</html> 
+</html>

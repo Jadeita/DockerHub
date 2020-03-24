5c688622aac30879b314190bab96cd898fe745d1
Jade Bañuelos <joibg.ver@gmail.com>
2020-03-24 16:31:05 -0600

    1

--- docker/miweb/index.php
+++ docker/miweb/index.php
@@ -5,8 +5,11 @@
 	<meta charset="utf-8">
 </head>
 
-<body background="https://lh4.googleusercontent.com/fss7r2H9QJsR9yT6J8PxhIQDPg3if5p_mRgbK0U0Ep5Q1rsi17vobpHLzbXp1oPFrIwevGt4kKU9TcX82hfM2Xe6EiqkOnU8bAkBifsxhTM5nSRaMks=w1280">
+<div class="">
+  <body background="https://lh4.googleusercontent.com/fss7r2H9QJsR9yT6J8PxhIQDPg3if5p_mRgbK0U0Ep5Q1rsi17vobpHLzbXp1oPFrIwevGt4kKU9TcX82hfM2Xe6EiqkOnU8bAkBifsxhTM5nSRaMks=w1280">
 
-</body>
+  </body>
+</div>
+
 
 </html>

d85f8d03ed13da2a408412b6a829ff8cd60a797f
Jade Bañuelos <joibg.ver@gmail.com>
2020-03-24 15:48:04 -0600

    design

--- docker/miweb/index.php
+++ docker/miweb/index.php
@@ -4,17 +4,9 @@
 	<title> WebServices </title>
 	<meta charset="utf-8">
 </head>
-<body background="https://www.solofondos.com/wp-content/uploads/2015/11/fondo-celeste-oscuro-6269.jpg">
-  <font color="#FEAEE2" size="5" >
-  <center>
-<h1>Titular de tamaño h1 </h1>
-  </center>
 
-<h2>Titular de tamaño h2</h2>
-<h3>Titular de tamaño h3</h3>
-<h4>Titular de tamaño h4</h4>
-<h5>Titular de tamaño h5</h5>
-<h6>Titular de tamaño h6</h6>
+<body background="<?php echo base_url(); ?>imagenes/imagen1.png">
+
 </body>
 
 </html>
